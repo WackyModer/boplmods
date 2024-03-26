372a6a2b3258378a286f8c0bd6dce533c7c7fa53
WackyModer <wackymoder@feds.contact>
2024-03-25 22:00:22 -0700

    added proper blocking

--- src/index.js
+++ src/index.js
@@ -1,24 +1,49 @@
 const express = require('express');
 const port = 3000;
 const app = express();
+
 var wiki = require('./server/wiki.js');
+var blocking = require('./server/blocking.js');
+
+var fs = require('fs');
+
 var os = require('os');
 
-app.use((req,res) => {
+// setup stuff
+
+if(!fs.existsSync(__dirname+"../../storage/misc/blockedInfo.json")) {
+    fs.writeFileSync(__dirname+"../../storage/misc/blockedInfo.json", JSON.stringify(
+    {  
+        "bans": {
+            "emails": {},
+            "ip": {}
+        },
+        "ipBanSchema": {
+            "utcTimeBanned": 232323,
+            "reasonBanned": "",
+            "lengthBanned": "",
+            "logHistory": ""
+        }
+    }));
+}
+
+
+app.use((req,res, next) => {
     const clientIPv4 = req.headers['cf-connecting-ip'];
-    const clientIPv6 = req.headers['​​cf-connecting-ipv6']; // May be undefined
-    const clientIPv4_2 = req.headers['cf-pseudo-ipv4']; // May be undefined
     const clientIPv4_3 = req.headers['x-forwarded-for'];
 
-    if(clientIPv4 == "103.102.228.228"){
-        res.send("Something has gone VERY wrong.<br><br>DM \'wackymoder\' on discord with the error code 1003").status(200);
-        console.log("hoe tried");
+    var curDate = new Date();
+
+    if(blocking.isBlocked(clientIPv4, req, res)) {
+        // If they're blocked, just return
+        console.log(`${curDate.toUTCString()} | Blocked connection ${clientIPv4} | at resource ${req.path} | UA ${req.headers['user-agent']}`)
         return;
     }
 
-    console.log(`New connection, ${clientIPv4} | ${clientIPv4_3} | at resource ${req.path} | UA ${req.headers['user-agent']}`);
-    res.send("leave.");
-    res.status(200);//406)
+    console.log(`${curDate.toUTCString()} | New connection, ${clientIPv4} | ${clientIPv4_3} | at resource ${req.path} | UA ${req.headers['user-agent']}`);
+    
+    res.send("<h2 style=\"font-family: arial;\">You aren't meant to be here yet D:</h2>")
+    res.status(200);
     return;
 })
 
@@ -32,7 +57,7 @@ app.get('/wikiData/:page', wiki.returnPageRaw);
 app.post('/wikiData/createPage/:pageName', wiki.createPage);
 
 app.get('/server/uptime', (req, res) => {
-    res.send(`Server has been up for ${process.uptime().toFixed(0)} seconds, so it is ${(43200-process.uptime()).toFixed(0)} seconds from updating (Updates every 12 hours)
+    res.send(`Server has been up for ${process.uptime().toFixed(0)} seconds, so it is ${(21600-process.uptime()).toFixed(0)} seconds from updating (Updates every 6 hours)
     <br><br>
     Current usages are: ${(100-os.freemem()/os.totalmem()*100).toFixed(1)}% (${((os.totalmem()-os.freemem())/1024/1024/1024).toFixed(1)} GB) mem usage (theres not that much mem)`)
     res.status(200);
@@ -81,7 +106,7 @@ app.use(function(req, res, next) {
 app.listen(port, () => {
     console.log(`Express server has started on port ${port}`)
 });
-
+/*
 function fuckingSleepPls(ms) {
     return new Promise((resolve) => {
         setTimeout(resolve, ms);
@@ -133,3 +158,5 @@ async function backupLoop() {
 
 //console.log("Entering backup loop");
 //backupLoop();
+
+*/
---
+++ src/server/blocking.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+// Path to the JSON file
+const bansFilePath = __dirname+'../../../storage/sensitive/blockedInfo.json';
+
+function isBlocked(clientIPv4, req, res) {
+    var blockList = fs.readFileSync(bansFilePath);
+    blockList = JSON.parse(blockList);
+
+    var banObject = blockList.bans.ip[clientIPv4];
+
+    if(!banObject) {
+        if(req.url.endsWith('.php') || req.url.contains("wp-includes")) {
+            banObject = {
+                "utcTimeBanned": (Date.now()/1000).toFixed(0),
+                "reasonBanned": "Seemingly trying to find a .php file or wordpress crap. Pretty sketch, don't do it.",
+                "lengthBanned": 2592000,
+                "logHistoryAfterBan": `${new Date().toUTCString()} | Blocked connection ${clientIPv4} | at resource ${req.path} | UA ${req.headers['user-agent']}\n`
+            };
+
+            blockList.bans.ip[clientIPv4] = banObject;
+
+            fs.writeFileSync(bansFilePath, JSON.stringify(blockList, null, 2));
+
+            res.send(`You have been **IP BANNED** for 30 days with the reason \"${banObject.reasonBanned}\". If this has happened by accident contact \"wackymoder\" on Discord OR wackymoder@feds.contact`);
+            res.status(200);
+            return true;
+        }
+    } else {
+        banObject.logHistoryAfterBan += `${Date.now()} | Blocked connection ${clientIPv4} | at resource ${req.path} | UA ${req.headers['user-agent']}\n`
+        fs.writeFileSync(bansFilePath, JSON.stringify(blockList, null, 2));
+
+        res.send(`You are currently **IP BANNED** for ${((banObject.utcTimeBanned - Date.now()/1000 + banObject.lengthBanned) / 86400).toFixed(0)} days with the reason \"${banObject.reasonBanned}\". If this has happened by accident contact \"wackymoder\" on Discord OR wackymoder@feds.contact`);
+        res.status(200);
+        return true;
+    }
+
+    return false;
+}
+
+
+/*
+    "ipBanSchema": {
+        "utcTimeBanned": 232323,
+        "reasonBanned": "",
+        "lengthBanned": "",
+        "logHistory": ""
+    }
+*/
+module.exports = {
+    isBlocked
+}
